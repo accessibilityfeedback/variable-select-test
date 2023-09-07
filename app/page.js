9153ca8e112d283188e0e5f02db9b7e2585c88f3
codingmarisa <codingmarisa@gmail.com>
2023-09-07 11:08:39 -0700

    ADD Max num

--- app/page.js
+++ app/page.js
@@ -3,12 +3,16 @@
 import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 
+let startingNum = 1000;
+let max = 9999;
+
 export default function Home() {
-  const [numOptions, setNumOptions] = useState(1000);
+  const [numOptions, setNumOptions] = useState(startingNum);
   const [numOptionsArr, setNumOptionsArr] = useState([]);
+  const [overMax, setOverMax] = useState(false);
 
   useEffect(() => {
-    let newArr = getUniqueRandomNums(1000);
+    let newArr = getUniqueRandomNums(startingNum);
     setNumOptionsArr(newArr);
   }, []);
 
@@ -23,16 +27,32 @@ export default function Home() {
 
   const handleChange = (event) => {
     let val = event.target.value.replace(/\D/, '');
-    let newArr = getUniqueRandomNums(val);
-    setNumOptions(val);
-    setNumOptionsArr(newArr);
+    if (val <= max) {
+      setOverMax(false);
+      let newArr = getUniqueRandomNums(val);
+      setNumOptions(val);
+      setNumOptionsArr(newArr);
+    } else {
+      setOverMax(true);
+      let newArr = getUniqueRandomNums(max);
+      setNumOptions(val);
+      setNumOptionsArr(newArr);
+    }
   };
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <form>
+          <div>
+            <p>Input a Number from 1 to 9999</p>
+          </div>
           <div className={styles.formDiv}>
+            {overMax && (
+              <div className={styles.overMax}>
+                <span>Max value exceeded. Demo select populated to {max}.</span>
+              </div>
+            )}
             <label htmlFor="numOptions" className={styles.inputLabel}>
               Number of Select Options
             </label>
@@ -53,17 +73,36 @@ export default function Home() {
               id="optionName"
               className={styles.inputSelect}
             >
-              {numOptionsArr.map((option, i) => {
-                return (
-                  <option
-                    value={i + 1}
-                    key={option}
-                    className={styles.inputOption}
-                  >
-                    Number {i + 1}
-                  </option>
-                );
-              })}
+              {numOptions > 0 ? (
+                numOptionsArr.map((option, i) => {
+                  if (i === 0) {
+                    return (
+                      <option
+                        value={i + 1}
+                        key={option}
+                        className={styles.inputOption}
+                        selected
+                      >
+                        Number {i + 1}
+                      </option>
+                    );
+                  } else {
+                    return (
+                      <option
+                        value={i + 1}
+                        key={option}
+                        className={styles.inputOption}
+                      >
+                        Number {i + 1}
+                      </option>
+                    );
+                  }
+                })
+              ) : (
+                <option hidden className={styles.inputOption}>
+                  Set Number of Select Options
+                </option>
+              )}
             </select>
           </div>
         </form>
